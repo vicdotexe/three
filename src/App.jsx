db16083daa1e4fd31a4c4b606dade79cdeb7cdce
victor <vicdotexe@gmail.com>
2023-01-03 13:33:30 -0800

    playing with three.js cylinder math

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import World from "./World";
 
 export default () => (
   <>
     <h1>Welcome to React Vite Micro App!</h1>
     <p>Hard to get more minimal than this React app.</p>
+    <World/>
   </>
 );
